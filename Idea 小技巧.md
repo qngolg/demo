2d70c4f52c76502e218107102c30b8bb6fdb7137
Guoliang Qian <guoliangqian@CNfan.yang>
2019-03-04 13:22:56 +0800

    uml工具画法

---
+++ Idea 小技巧.md
@@ -0,0 +1,9 @@
+##给idea 添加UML插件 用于画类图，逻辑图
+https://plugins.jetbrains.com/plugin/7017-plantuml-integration
+
+#uml流程图画图小技巧
+https://github.com/frontend9/fe9-library/issues/79
+
+
+
+
---
+++ src/main/java/com/example/demo/stream/Demo.java
@@ -0,0 +1,10 @@
+package com.example.demo.stream;
+
+import java.util.stream.IntStream;
+
+public class Demo {
+
+    public void test(int num){
+        IntStream.range(0,num).mapToObj(i -> )
+    }
+}
